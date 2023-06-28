e89c9669d94b66fa841c3c7b98bb1497e8bc13b7
Erfan Saberi <erfansaberiow@gmail.com>
2023-06-28 03:44:16 +0330

    Changed STATIC_ROOT & MEDIA_ROOT

--- app/eduverse_backend/settings.py
+++ app/eduverse_backend/settings.py
@@ -132,10 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
-MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 
 # Default primary key field type
